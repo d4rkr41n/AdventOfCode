3d2c7d2ef7ecb6ca9ccccba4f2552f7103eaf98e
rain <rain@puddle.local>
2023-12-11 16:45:35 -0600

    Day 9 done

---
+++ 2023/day09/solutions/python1.py
@@ -0,0 +1,19 @@
+with open("../example.txt", "r") as fp:
+  inputs = fp.read().strip().split("\n")
+for i, inp in enumerate(inputs):
+  inputs[i] = list(map(int,inputs[i].split()))
+
+sum = 0
+for seq in inputs:
+  rows = []
+  while len(seq) > 0 and set(seq) != {0}:
+    rows.append(seq)
+    tmp = seq
+    seq = []
+    for i in range(len(tmp) - 1):
+      seq.append(tmp[i + 1] - tmp[i])
+
+  for r in rows:
+    sum += r[-1]
+
+print(sum)
